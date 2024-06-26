bc73e1d6c247f52e9071a8f0a83d0f89c7ef9c6f
Juraci Hein <juraci.hein@esx.com.br>
2024-06-26 19:25:32 +0000

    Updated App.java

--- src/main/java/com/devsecops/App.java
+++ src/main/java/com/devsecops/App.java
@@ -1,13 +1,36 @@
 package com.devsecops;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+
+    @Value("${azure.storage.connection-string}")
+    private String azureStorageConnectionString;
+
+    @Value("${azure.storage.container-name}")
+    private String azureStorageContainerName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public void listBlobs() {
+        // Criar um cliente para o container Blob
+        BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
+                .connectionString(azureStorageConnectionString)
+                .containerName(azureStorageContainerName)
+                .buildClient();
+
+        // Listar blobs dentro do container
+        System.out.println("Blobs no container '" + azureStorageContainerName + "':");
+        blobContainerClient.listBlobs().forEach(blobItem -> {
+            System.out.println(blobItem.getName());
+        });
     }
 }
