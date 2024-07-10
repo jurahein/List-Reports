3e5577801ae6cfb713531c60851457381ad29e8c
Automated Process <juraci.hein@esx.com.br>
2024-07-10 15:37:53 -0300

    incluindo paginas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-﻿// HomeController.cs
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using System.IO;
+using System.Collections.Generic;
 
 namespace AzureStorageListFiles.Controllers
 {
@@ -15,21 +15,36 @@ namespace AzureStorageListFiles.Controllers
             _blobServiceClient = blobServiceClient;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-            var blobs = await ListBlobsAsync("files");
+            return View();
+        }
+
+        public async Task<IActionResult> OwaspReports()
+        {
+            var blobs = await ListBlobsAsync("owaspreports");
             return View(blobs);
         }
 
-        public async Task<IActionResult> Download(string fileName)
+        public async Task<IActionResult> SnykReports()
         {
-            var containerClient = _blobServiceClient.GetBlobContainerClient("files");
-            var blobClient = containerClient.GetBlobClient(fileName);
+            var blobs = await ListBlobsAsync("snykreports");
+            return View(blobs);
+        }
 
+        public async Task<IActionResult> SonarReports()
+        {
+            var blobs = await ListBlobsAsync("sonarreports");
+            return View(blobs);
+        }
+
+        public async Task<IActionResult> Download(string fileName, string containerName)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            var blobClient = containerClient.GetBlobClient(fileName);
             var memoryStream = new MemoryStream();
             await blobClient.DownloadToAsync(memoryStream);
             memoryStream.Position = 0;
-
             return File(memoryStream, "application/octet-stream", fileName);
         }
 
@@ -37,12 +52,10 @@ namespace AzureStorageListFiles.Controllers
         {
             var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
             var files = new List<string>();
-
             await foreach (var blobItem in containerClient.GetBlobsAsync())
             {
                 files.Add(blobItem.Name);
             }
-
             return files;
         }
     }
